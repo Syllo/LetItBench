bac12846c3b897db55ef49ecbfd76b942804faaf
Maxime Schmitt <max.schmitt@unistra.fr>
2019-04-16 11:28:40 +0200

    Plot script: add values on bars

--- helper_scripts/plot_gathered_script.hs
+++ helper_scripts/plot_gathered_script.hs
@@ -27,11 +27,11 @@ import qualified Data.Map as M
 import           Data.Map (Map)
 import qualified Data.Set as S
 import           Data.Set (Set, member)
-import           Data.Maybe (catMaybes, maybe, fromJust, isNothing)
+import           Data.Maybe (catMaybes, maybe, fromJust, isNothing, fromMaybe)
 import qualified Data.Text.ICU as ICU
 import           Data.Text.ICU (Regex)
 import           Control.Monad (foldM)
-import           Data.List (foldl', unzip)
+import           Data.List (foldl', unzip, intercalate)
 
 type BenchmarkName = Text
 type DataName = Text
@@ -48,6 +48,8 @@ data CmdArguments = CmdArguments { inputDataFile :: String
                                  , filterData :: [Text]
                                  , plotSize :: (Double, Double)
                                  , addSpeedupFrom :: [(Text,Text,Text)]
+                                 , labelOverBarRotation :: Maybe Int
+                                 , labelOverBarOffset :: Maybe Int
                                  } deriving (Show, Data)
 
 
@@ -55,12 +57,14 @@ cmdLineParser :: IO (CmdArguments)
 cmdLineParser = do
   progName <- getProgName
   return $ CmdArguments { inputDataFile = mempty &= (ARG.argPos 0) &= ARG.typ "InputDataFile"
-                        , outFile = mempty &= ARG.name "o" &= ARG.help "Output file name"
-                        , mplStyle = mempty &= ARG.name "S" &= ARG.help "Matplotlib style"
+                        , outFile = Nothing &= ARG.name "o" &= ARG.help "Output file name"
+                        , mplStyle = Nothing &= ARG.name "S" &= ARG.help "Matplotlib style"
                         , filterBench = mempty &= ARG.name "b" &= ARG.help "Match only bench line name by regex (e.g., -b a -b b to mach a or b)"
                         , filterData = mempty &= ARG.name "d" &= ARG.help "Match only data column name by regex (e.g., -d a -d b to mach a or b)"
                         , plotSize = (210, 297) &= ARG.name "p" &= ARG.help "Plot size (x,y) in mm"
                         , addSpeedupFrom = mempty &= ARG.name "s" &= ARG.help "Compute the speedup of (a,b,c), where a is the column selector, b is the row of reference and c is helps selecting the rows from which to compute the speedup from"
+                        , labelOverBarRotation = Nothing &= ARG.name "r" &= ARG.help "Rotation in degrees of the label over the bar"
+                        , labelOverBarOffset = Nothing &= ARG.name "l" &= ARG.help "Rotation in degrees of the label over the bar"
                         } &= ARG.summary "GatherScript v1.0"
                           &= ARG.program progName
 
@@ -80,7 +84,7 @@ main = do
                                     speedupRegex <- mapM compileSpeedup $ addSpeedupFrom args
                                     dataMapWithSpeedup <- foldM addSpeedupTo dataMap speedupRegex
                                     pure $ (benchRegex, dataRegex ++ regexForSpeedup (addSpeedupFrom args), dataMapWithSpeedup)
-  either printErrorAndFail (plotDataAsBar plotInchSize (mplStyle args) outputFile) eitherDataMapWithSpeedup
+  either printErrorAndFail (plotDataAsBar plotInchSize (mplStyle args) outputFile (labelOverBarRotation args) (labelOverBarOffset args)) eitherDataMapWithSpeedup
   where
     printErrorAndFail str = printError str >> exitFailure
     printError = hPutStrLn stderr
@@ -172,14 +176,18 @@ textToDouble txt = case Data.Text.Read.double txt of
                       Left a -> Left $ "The gathered data values should be floating point: " `mappend` a
                       Right (dval , _) -> Right dval
 
-plotDataAsBar :: (Double, Double) -> Maybe PlotStyle -> Maybe String -> ([BenchFilterRegex], [DataFilterRegex], DataMap) -> IO ()
-plotDataAsBar (sizeX, sizeY) pltStyle outputFile (benchNamePattern, dataNamePattern, dmap) = plotProcessor
-                                                                                       $ plotStyle
-                                                                                       % PLT.figure @@ [o2 "num" (1 :: Int), o2 "clear" True]
-                                                                                       % genMultiBarPlots numPlotRows numPlotCols dataNames benchNames dmap
-                                                                                       % PLT.setSizeInches sizeX sizeY
-                                                                                       % PLT.tightLayout
+plotDataAsBar :: (Double, Double) -> Maybe PlotStyle -> Maybe String -> Maybe Int -> Maybe Int -> ([BenchFilterRegex], [DataFilterRegex], DataMap) -> IO ()
+plotDataAsBar (sizeX, sizeY) pltStyle outputFile
+              labelOverRotate labelOverOffset
+              (benchNamePattern, dataNamePattern, dmap) = plotProcessor
+                                                        $ plotStyle
+                                                        % labelOverBarHelperFun @@ [o2 "rotation" labelRotate]
+                                                        % PLT.figure @@ [o2 "num" (1 :: Int), o2 "clear" True]
+                                                        % genMultiBarPlots numPlotRows numPlotCols labelOverOffset dataNames benchNames dmap
+                                                        % PLT.setSizeInches sizeX sizeY
+                                                        % PLT.tightLayout
   where
+    labelRotate = fromMaybe 90 labelOverRotate
     plotProcessor = maybe PLT.onscreen (\file -> (\pdef -> PLT.file file pdef >> return mempty)) outputFile
     --plotProcessor a = PLT.code a >>= \b -> putStrLn b
     filterByName [] = id
@@ -191,21 +199,56 @@ plotDataAsBar (sizeX, sizeY) pltStyle outputFile (benchNamePattern, dataNamePatt
 
 barWithErrs :: (ToJSON x, ToJSON y, ToJSON err) => x -> y -> err -> Matplotlib
 barWithErrs xlabel height err = PLT.readData (xlabel, height, err) % PLT.mp # "ax.bar(data[0], data[1], yerr=data[2] " ## ")"
+                              % PLT.mp # "ax.set_xticklabels(data[0]" ## ")" @@ [o2 "rotation" (40::Int), o2 "horizontalalignment" "right", o2 "family" "sans-serif"]
 
-genMultiBarPlots :: Int -> Int -> [DataName] -> [BenchmarkName] -> DataMap -> Matplotlib
-genMultiBarPlots nRows nCols dns bns dmap =
+genMultiBarPlots :: Int -> Int -> Maybe Int -> [DataName] -> [BenchmarkName] -> DataMap -> Matplotlib
+genMultiBarPlots nRows nCols labelOffset dns bns dmap =
   mconcat [   PLT.addSubplot nRows nCols plotId
             % PLT.title (T.unpack plotTitle)
-            % barWithErrs labels ((flip (-) bottomPlot) <$> meanData) stdErrData @@ [o2 "bottom" bottomPlot]
-            % PLT.axisXTickLabels (PLT.raw . T.unpack <$> labels) @@ [o2 "rotation" (40::Int), o2 "horizontalalignment" "right", o2 "family" "sans-serif"]
+            % barWithErrs labels ((flip (-) bottomOffset) <$> meanData) stdErrData @@ [o2 "bottom" bottomOffset]
+            % plotLabelOverBar labelOffset bottomOffset
           | (plotId, plotTitle) <- ([1..] :: [Int]) `zip` dns
           , let (labels, dataVals) = unzip . catMaybes $ dataFrom bns plotTitle
           , let currData = toStatData <$> dataVals
           , let meanData = rawMean <$> currData
           , let stdErrData = errorBarValues <$> currData
-          , let bottomPlot = max 0 (minimum (zipWith (-) meanData stdErrData) - (maximum meanData - minimum meanData) * 0.1)
+          , let bottomOffset = max 0 (minimum (zipWith (-) meanData stdErrData) - (maximum meanData - minimum meanData) * 0.1)
           ]
   where
     dataFrom :: [Text] -> Text -> [Maybe (Text, Vector Double)]
     dataFrom benchNames dataName = [Just ((,) bName) <*> (M.lookup (bName,dataName) dmap) | bName <- benchNames]
     errorBarValues = (maybe 0 statStdDev) . multiRawStat
+
+labelOverBarHelperFun :: Matplotlib
+labelOverBarHelperFun = PLT.mp # intercalate "\n" [ "def add_value_labels(ax, xVals, yVals, bottomOffset, spacing=5):"
+                                                  , "    (y_min,y_max) = ax.get_ylim()"
+                                                  , "    for x_value, y_value in zip(xVals,yVals):"
+                                                  , "        space = spacing"
+                                                  , "        if y_value < 0:"
+                                                  , "          if y_value < 0.25 * (y_max - y_min):"
+                                                  , "            va = 'bottom'"
+                                                  , "          else:"
+                                                  , "            space = -space"
+                                                  , "            va = 'top'"
+                                                  , "        else:"
+                                                  , "          if y_value > 0.75 * (y_max - y_min):"
+                                                  , "            va = 'top'"
+                                                  , "            space = -space"
+                                                  , "          else:"
+                                                  , "            va = 'bottom'"
+                                                  , "        y_value = y_value + bottomOffset"
+                                                  , "        y_abs = abs(y_value)"
+                                                  , "        if y_abs >= 10 and y_abs < 1000:"
+                                                  , "          label = \"{:.2f}\".format(y_value)"
+                                                  , "        elif y_abs >= 100 and y_abs < 10000:"
+                                                  , "          label = \"{:.1f}\".format(y_value)"
+                                                  , "        else:"
+                                                  , "          label = \"{:.3g}\".format(y_value)"
+                                                  , "        ax.annotate(label, (x_value, y_value), xytext=(0, space), textcoords=\"offset points\", ha='right', va=va"
+                                                  ]
+                               ## ")"
+
+plotLabelOverBar :: Maybe Int -> Double -> Matplotlib
+plotLabelOverBar pointSpace bottomOffset = PLT.mp # "add_value_labels(ax, data[0], data[1], " # bottomOffset # ", spacing=" # spaceVal # ")"
+  where spaceVal = fromMaybe 5 pointSpace
+
